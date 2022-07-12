1845cd431bec43a8fc6457a5ed590f73180d020f
Michael Hernández <mghernandezcastillo@gmail.com>
2022-07-11 22:01:04 -0500

    update

--- inventory_be/inventoryApp/models/report.py
+++ inventory_be/inventoryApp/models/report.py
@@ -11,7 +11,7 @@ class Report(models.Model):
     dateTime = models.DateTimeField(default= datetime.datetime.now())
     status = models.CharField(max_length=10)
     observation = models.TextField(max_length=100, null=True, blank=True)
-    employee = models.CharField(max_length=50)
+    employee = models.CharField(max_length=500)
 
     def __str__(self):
         return str(self.id) + ' ' + str(self.item) + ' ' + str(self.dateTime) + ' ' + str(self.status) + ' ' + str(self.observation) + ' ' + str(self.employee)
